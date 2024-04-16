5804d1db77c44da924319ff31d474de479e1adeb
Abdullah Bedwey <bedweyy@gmail.com>
2024-04-16 21:30:39 +0200

    solve all task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,16 +27,17 @@ class Auth:
         :return: False if the path is in the list of excluded paths,
         True otherwise
         """
-        if path is None:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
 
-        if path[-1] != '/':
-            path += '/'
-
-        return path not in excluded_paths
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
