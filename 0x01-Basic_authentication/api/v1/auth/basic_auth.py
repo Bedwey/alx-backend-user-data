dd01fb109d09059f35963947083354fb49977b61
Abdullah Bedwey <bedweyy@gmail.com>
2024-04-16 21:23:03 +0200

    Basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ serves as a template for basic authentication systems.
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +57,48 @@ class BasicAuth(Auth):
                 .decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded value.
+
+        :param decoded_base64_authorization_header: a Base64 decoded string
+        :return: (None, None) if decoded_base64_authorization_header is None,
+        not a string, or doesn't contain a colon,
+        otherwise the user email and password as a tuple
+        """
+        if decoded_base64_authorization_header is None or \
+                type(decoded_base64_authorization_header) is not str or \
+                ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        return credentials[0], credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his email and password.
+
+        :param user_email: a string representing the user's email
+        :param user_pwd: a string representing the user's password
+        :return: None if user_email or user_pwd is None or not a string,
+        if the database doesn't contain a User instance with email equal to user_email,
+        or if user_pwd is not the password of the User instance found,
+        otherwise the User instance
+        """
+        if user_email is None or type(user_email) is not str or \
+                user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
