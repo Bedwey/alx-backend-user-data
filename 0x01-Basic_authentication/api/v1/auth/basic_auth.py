31887093e24b124e872b735f4d0f351c56cb1c25
Abdullah Bedwey <bedweyy@gmail.com>
2024-04-16 21:07:57 +0200

    typo again

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,23 +13,22 @@ class BasicAuth(Auth):
     It inherits from the Auth class.
     """
 
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the
+        Authorization header for a Basic Authentication.
 
-def extract_base64_authorization_header(
-        self, authorization_header: str) -> str:
-    """
-    Extracts the Base64 part of the
-    Authorization header for a Basic Authentication.
+        :param authorization_header: a string
+        representing the Authorization header
+        :return: None if the authorization_header is None,
+        not a string, or doesn't start with "Basic ",
+        otherwise the part of the string after "Basic "
+        """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
 
-    :param authorization_header: a string
-    representing the Authorization header
-    :return: None if the authorization_header is None,
-    not a string, or doesn't start with "Basic ",
-    otherwise the part of the string after "Basic "
-    """
-    if authorization_header is None or type(authorization_header) is not str:
-        return None
+        if not authorization_header.startswith('Basic '):
+            return None
 
-    if not authorization_header.startswith('Basic '):
-        return None
-
-    return authorization_header.split(' ', 1)[1]
+        return authorization_header.split(' ', 1)[1]
